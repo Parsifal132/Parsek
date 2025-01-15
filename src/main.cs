d50f129fe92c6bc0598fa5a3ed8f566a43235ba0
Parsifal132 <20uchenik04@gmail.com>
2025-01-15 21:34:56 +0300

    After add-numbers

---
+++ src/main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class main
+{
+    static void Main()
+    {
+        try
+        {
+            int a, b;
+            Console.WriteLine("Введите а");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            b = Convert.ToInt32(Console.ReadLine());
+            Utils.utils.add_numbers(a, b);
+
+        }
+        catch (Exception e) { 
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
---
+++ src/utils.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class utils
+    {
+        public static void add_numbers(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine(a+"+"+b+"="+c);
+        }
+        public static void multiply_numbers(int a, int b)
+        {
+            int c = a * b;
+            Console.WriteLine(a + "*" + b + "=" + c);
+        }
+    }
+}
